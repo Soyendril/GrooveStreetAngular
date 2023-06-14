79e531b03544a93c0757015cb7f165e1a6a68890
Soyendril <souleymane.rakkassy@gmail.com>
2023-06-14 15:06:29 +0200

    page ajout photo et bio + routing des pages

--- src/app/Pages/ajout-photo-page/ajout-photo-page.component.ts
+++ src/app/Pages/ajout-photo-page/ajout-photo-page.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/authentification/services/auth.service';
+import { MusicienService } from '../Services/musicien.service';
+
+import Musicien from 'src/app/authentification/model/musicien.model';
 
 @Component({
   selector: 'app-ajout-photo-page',
@@ -6,5 +10,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./ajout-photo-page.component.css']
 })
 export class AjoutPhotoPageComponent {
+  
+  musicien: Musicien ={
+    id: null,
+    nom: '',
+    pseudo: '',
+    password: '',
+    email:'',
+    style : '',
+    description: '',
+    photo: '',
+    codePostal: '',
+    age: undefined
+  };
+  photoMusicien?: string;
+  pseudoMusicien?: string;
+
+  constructor(
+    private authService: AuthService,
+  ) { }
+
+
+  ngOnInit(): void {
+    this.authService.autoLogin();
+    const userID = this.authService.getId();
+  
+
+  if (userID) {
+    const userObject = JSON.parse(userID);
+    this.pseudoMusicien = userObject.pseudo;
+    this.photoMusicien = userObject.photo;
+    console.log(userObject.photo);
+  }
+}
+
 
 }
